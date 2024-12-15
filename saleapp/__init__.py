4b07034868b48043a0ceac0ee661ee91e2bbea17
truongtiendat19 <truongtiendat300@gmail.com>
2024-12-15 23:56:15 +0700

    dat

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -1,2 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Tiendat1@@localhost/saledb?charset=utf8mb4"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
