e9de720eebe386dc742406e973ef63c7c72afd9c
truongtiendat19 <truongtiendat300@gmail.com>
2024-12-15 22:48:42 +0700

    dat

--- saleapp/index.py
+++ saleapp/index.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, request
+from saleapp import app
+from flask import render_template, request
 from pyexpat.errors import messages
-import dao
-app = Flask(__name__)
+from saleapp import dao
 
 @app.route("/")
 def index():
     cates = dao.load_categories()  # Gọi hàm lấy danh mục từ dao.py
     return render_template('index.html', categories=cates)
 
-@app.route("/dangnhap")
-def dangnhap():
-    return render_template('dangnhap.html')
+@app.route("/login", methods=['get', 'post'])
+def login():
+    return render_template('login.html')
 
+@app.route("/login1", methods=['get', 'post'])
+def login1():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'admin' and password == '123':
+            return 'successful'
+    return 'failed'
 
 @app.route("/hello/<name>")
 def hello(name):
     return render_template('index.html',message="XIN CHÀO %s" %name)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
