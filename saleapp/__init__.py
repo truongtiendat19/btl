f93aac230242a03536a73324fc6ce0f5dd35de3e
truongtiendat19 <truongtiendat300@gmail.com>
2024-12-16 17:35:25 +0700

    Update __init__.py

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456789@localhost/saledb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
-
 db = SQLAlchemy(app=app)
 admin = Admin(app=app, name="QUẢN LÝ BÁN SÁCH",template_mode="bootstrap3")
 
